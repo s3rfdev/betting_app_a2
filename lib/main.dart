b68b8be9b6b9b07d1eab072fc916a4ce51423718
Sergei Fomenko <130678664+s3rfdev@users.noreply.github.com>
2023-07-17 17:55:50 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -39,7 +39,7 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  WebViewController controller = WebViewController();
+  WebViewController controller = WebViewController()..setJavaScriptMode(JavaScriptMode.unrestricted);
   bool isVisible = false;
   String initialHost = '';
   String webviewUrl = '';
